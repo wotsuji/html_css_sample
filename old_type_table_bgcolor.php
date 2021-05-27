ece78032bad711dadf67b42e1fe79da22f36d8bc
wataru.otsuji <wataru.otsuji@gmail.com>
2021-05-27 11:19:39 +0900

    NG例を更新

--- old_type_table_bgcolor.php
+++ old_type_table_bgcolor.php
@@ -3,6 +3,8 @@
 </head>
 <body>
 
+<h1>昔ながらのテーブル背景切り替え</h1>
+
 <table>
 <?php
 for ($i=1; $i<10; $i++) {
@@ -21,6 +23,27 @@ if ( $i % 2 === 0) {
 }
 ?>
 </table>
+
+<textarea rows="10" cols="60">
+<table>
+<?php
+for ($i=1; $i<10; $i++) {
+?>
+<?php
+$bgcolor = "ffffff";
+if ( $i % 2 === 0) {
+  $bgcolor = "c0c0e0";
+}
+?>
+<tr style="background-color: <?php echo $bgcolor; ?>">
+ <td>hoge</td>
+ <td>fuga</td>
+</tr>
+<?php
+}
+?>
+</table>
+</textarea>
 
 </body>
 </html>
